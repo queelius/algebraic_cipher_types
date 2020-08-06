63f1717bfd47c1d6b1e2be57e4d855bbe94b0cea
Alex Towell <queelius@gmail.com>
2020-08-06 05:38:08 -0500

    updates

---
+++ include/cipher_if.hpp
@@ -0,0 +1,194 @@
+#pragma once
+
+#include <memory>
+using std::shared_ptr;
+
+
+// type-erase model of the the concept
+// template<>
+// class cipher<cipher<bool>,cipher<T>>
+
+// cipher if : cipher bool -> T -> T
+// this means that 'if' is a cipher map
+// that accepts a cipher bool and returns
+// a value of type T.
+//
+//
+// suppose the original if expression was
+// 
+// let E := (if expr -1 1)
+// then, E 1 = -1
+// and   E 0 = 1.
+//
+// (cipher E) maps E to a cipher
+// expression in which the code
+// (if expr -1 1) is obscured to
+// cipher E := (if expr a' b')
+// such that (cipher E) 1 = -1
+//       and (cipher E) 0 = 1.
+// now, we also wish to pass
+// a cipher bool as the expr
+// instead of a bool.
+// 
+// 
+// 
+
+// let's also type-erase any cipher that
+// models a cipher bool to cipher<bool>.
+
+struct cipher_type {};
+
+template <typename T = cipher_type>
+class cipher_if
+{
+public:
+    using input_type = cipher<bool>;
+    using output_type = T;
+
+    auto operator()(cipher<bool> e) const
+    {
+        // f is a cipher map on cipher[bool]
+        return if_->eval(e);
+    }
+
+private:
+    struct concept
+    {
+        virtual T eval(cipher<bool> e) const = 0;
+    };
+
+    shared_ptr<concept const> if_;
+
+    template <typename CipherMap>
+    struct model final : concept
+    {
+        CipherMap if_;
+
+        cipher<T> eval(cipher<bool> e) const
+        {
+            return if_(e);
+        }
+    };
+};
+
+
+
+
+/**
+ * cipher<bool> models the concept of an
+ * approximate cipher Boolean.
+ * 
+ * it does not support equality operations;
+ * rather, a secret must be known to decode
+ * to a plaintext bool to perform such queries.
+ */
+
+/**
+ * process;
+ * 
+ * specify input type X
+ * specify output type Y
+ * 
+ * specify (cipher) representations for X (cipher X)
+ * specify (cipher) representations for Y (cipher Y)
+ * 
+ * define function of f: X -> Y
+ * 
+ * for each x in X:
+ *     y := f(x)
+ *     [y'] := cipher(y)
+ *     ...
+ * 
+ * 
+ * can use a canonical representation of a body
+ * e.g., renaming variables to some canonical form,
+ * renaming function names to some canoncial form,
+ * etc., and then hashing it, to quickly check that
+ * two ciphers (say, a cipher of a function) model
+ * the same thing. same for types. in this way,
+ * we can verify that two, say, cipher bools are
+ * of the same type, i.e., same ciphers for true and
+ * false.
+ * 
+ * 
+ */
+
+
+struct cipher
+{
+    
+}
+
+template <>
+class cipher<bool>
+{
+public:
+    using plain_value_type = bool;
+
+    // make this a separate class?
+    using meta_info_type = map<string, variant<string,size_t,cipher_type>>;
+
+    auto fpr() const { return bool_->fpr(); }
+    auto fnr() const { return bool_->fnr(); }
+    auto size() const { return bool_->size(); }
+    auto meta_info() const { return bool_->meta_info(); }
+    auto try_convert(string_view secret) const
+    {
+        return bool_->try_convert(secret);
+    }
+
+private:
+    struct concept
+    {
+        virtual double fnr() const = 0;
+        virtual double fpr() const = 0;
+        virtual size_t size() const = 0;
+
+        // what should meta_info return?
+        // instead of string, return a map?
+        virtual meta_info_type meta_info() const = 0;
+        virtual optional<bool> try_convert(string_view) const = 0;
+    };
+
+    shared_ptr<concept const> bool_;
+
+    template <typename C>
+    struct model final : concept
+    {
+        size_t size() const { return size(c); }
+        double fnr() const { return fnr(c); }
+        double fpr() const { return fpr(c); }
+        string meta_info() const { return meta_info(c); }
+        optional<bool> try_convert(string_view secret) const
+        {
+            return try_convert(c,secret);
+        }
+
+        C c;
+    };
+};
+
+auto fnr(cipher<bool> const & x)
+{
+    return x.fnr();
+}
+
+auto size(cipher<bool> const & x)
+{
+    return x.size();
+}
+
+auto fpr(cipher<bool> const & x)
+{
+    return x.fpr();
+}
+
+auto meta_info(cipher<bool> const & x)
+{
+    return x.meta_info();
+}
+
+auto convert_to(cipher<bool> const & x, string_view secret)
+{
+    return x.try_convert(secret);
+}
--- include/trapdoor/trapdoor_set.hpp
+++ include/trapdoor/trapdoor_set.hpp
@@ -1,7 +1,113 @@
 #include "trapdoor.hpp"
 
-
 /**
+ * Consider the Boolean algebra
+ *     A := (P(X*), and, or, complement, {}, X*)
+ * where X is the alphabet and X* is the
+ * free semigroup on X which is closed under
+ * concatenation,
+ *     : : X* -> X* -> X*.
+ * 
+ * For example, if
+ *     X = {a,b}
+ * then
+ *     X* = {^, a, b, aa, ab,
+ *           ba, bb, aaa, aab, ... }
+ * and P(X*) is the power set of X*,
+ *     P(X*) = {{}, ^, {a}, {b},
+ *              {aa}, {a,aa}, {a,bb}, ... }.
+ * 
+ * Consider the Boolean algebra
+ *     B := ({0,1}^n, &, |, ~, 0^n, 1^n)
+ * and suppose we have a homomorphism
+ *     F : A -> B
+ * defined in the following way.
+ * 
+ * First, we have a cryptographic hash
+ * function
+ *     hash : X* -> {0,1}^n
+ * that a priori uniformly distributes
+ * over {0,1}^n, i.e., each X* maps to
+ * any element in the {0,1}^n with
+ * probability 2^(-n).
+ * 
+ * Then, homomorphism F maps strings
+ * in X* to bit strings in {0,1}^n
+ * by applying the hash function
+ * to the input concatenated with
+ * a secret s,
+ *     F a := hash(a s).
+ * 
+ * Note #1: Later, we generalize this
+ * to mapping each a in X* to multiple
+ * elements in {0,1}^n proportional to
+ * 1/P[a].
+ * 
+ * Observe that F is one-way, i.e., there
+ * is no homomorphism G such that
+ *     F G B = A.
+ * 
+ * Theorem:
+ * The morphism F defined as
+ * 
+ *     X*         := hash(a # s)
+ *     and        := &
+ *     or         := |
+ *     complement := ~
+ *     {}         := 0^n
+ *     X*         := 1^n.
+ * 
+ * is a homomorphism.
+
+ * Proof:
+ * 
+ * F ({} or A) = F {} | F A = 0^n | F A = F A
+ * 
+ * F ({} and A) = F {} & F A = 0^n & F A = 0^n
+ * 
+ * We wish to prove F (A or B) = F A + F B.
+ * F ({a} or {b}) = F {a} | F {b}
+ * 
+ * We wish to prove F (A and B) = F A + F B.
+ * F ({a} and {b}) = F {a} & F {b}
+ * 
+ * We wish to prove F (complement A) = ~ F A.
+ * F (complement {a}) = ~ F {a} 
+ * 
+ * qed
+ * 
+ * Since multiple elements in X* map to the same
+ * element in {0,1}^n, it is a homomorphism rather
+ * than an isomorphism.
+ * 
+ * What is the probability that two unique elements
+ * in X* map to the same element in {0,1}^n? That is
+ * to say, what is the probability of collision?
+ * Since F uniformly distributes over {0,1}^n, it
+ * is just
+ * 
+ *     P[x and y collide] = 2^(-n).
+ * 
+ * By the law of probability, therefore, the
+ * probability that they do not collide is just
+ * 
+ *     P[x and y do not collide] = 1 - 2^(-n).
+ * 
+ * Next, we define relations on sets. Set
+ * membership relation has a characteristic
+ * function
+ *    in : X -> 2^X -> bool
+ * which we define as
+ *    F in a b := a & b == a.
+ * 
+ * The subset relation has a predicate
+ *    subset : 2^X -> 2^X -> bool
+ * which we define as
+ *    F subset a b := a & b == a,
+ * just as with the characteristic function,
+ * although they have different probabilistic
+ * features.
+ *
  * If X = {a,b,c}, then 2^X = {{},{a},{b},{c},{a,b},{a,c},{b,c},{a,b,c}}.
  * 
  * A Boolean index over X is a Boolean algebra over 2^X with {}=0 and X=1
@@ -18,27 +124,19 @@
  */
 
 template <typename X>
-struct set_of {};
-
-template <typename X>
-struct trapdoor_set: public trapdoor<set_of<trapdoor<X>>>
+struct trapdoor_set
 {
     using value_type = X;
 
     trapdoor_set() :
-        cardinality(0),
         value_hash(0),
         key_hash(0)
     {
         // makes the empty set
     }
 
-    trapdoor_set(trapdoor_set const & copy) :
-        cardinality(copy.cardinality),
-        value_hash(copy.value_hash),
-        key_hash(copy.key_hash) {}
+    trapdoor_set(trapdoor_set const & copy) = default;
 
-    unsigned int cardinality;
     unsigned int value_hash;
     unsigned int key_hash;
 };
