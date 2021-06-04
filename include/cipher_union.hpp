c82fd46aecde21d12dc06b92029b3be71554c9b2
Alex Towell <queelius@gmail.com>
2021-06-04 04:35:24 -0500

    updates

--- include/cipher_union.hpp
+++ include/cipher_union.hpp
@@ -1,5 +1,3 @@
-
-
 /**
  * The sum type is an algebraic type that takes two types X and Y and produces
  * another type X + Y such that values of this type either come from X or Y.
@@ -21,6 +19,18 @@
  * it is (cipher X) or (cipher Y). That is to say, we must cipher (cipher X)
  * and cipher (cipher Y) and return those.
  * 
+ * 
+ * 
+ * The sum type A + B comes with two introduction forms (injections),
+ *     inj1: A -> A + B
+ * and
+ *     inj2: B -> A + B.
+ * The elimination form is case analysis. If e has type A + B and e1 and e2 have
+ * type T under the assumptions x: A and y: B respectively, then the term
+ *     case e of x -> e1
+ *               y -> e2
+ * has type T. The sum type corresponds to intuitionistic logical disjunction
+ * under the Curry–Howard correspondence.
  */
 
 
